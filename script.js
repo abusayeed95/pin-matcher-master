bb4ae1343f2984ebb2540bc828b5f229d0020fe0
abuSayeed995 <sayemsayeed.professional@gmail.com>
2020-07-31 18:08:09 +0600

    complex things done

--- script.js
+++ script.js
@@ -11,16 +11,35 @@ document.getElementById('pinGenerateBtn').addEventListener('click', function () 
 // Submission
 
 
-var codeDisplay = document.getElementById('codeDisplay').value;
-var codeDisplayValue = parseInt(codeDisplay);
-var keyPadDisplay = document.getElementById('keyPadDisplay').value;
-var keyPadDisplayValue = parseInt(keyPadDisplay);
-
 document.getElementById('submit').addEventListener('click', function () {
+    var codeDisplay = document.getElementById('codeDisplay').value;
+    var codeDisplayValue = parseInt(codeDisplay);
+    var keyPadDisplay = document.getElementById('keyPadDisplay').value;
+    var keyPadDisplayValue = parseInt(keyPadDisplay);
+
+    if (codeDisplay == '') {
+        alert('hey Mr.....! first generate a code')
+    }
+
     if (codeDisplayValue == keyPadDisplayValue) {
         document.getElementById('matched').style.display = 'block';
+        document.getElementById('notMatched').style.display = 'none';
     }
+
     if (codeDisplayValue !== keyPadDisplayValue) {
         document.getElementById('notMatched').style.display = 'block';
+        document.getElementById('matched').style.display = 'none';
+    }
+
+    document.getElementById('actionLeft').innerText = document.getElementById('actionLeft').innerText - 1;
+
+    if (document.getElementById('actionLeft').innerText == 1) {
+        alert('Think twice before submit... Only 1 try left');
+    }
+
+    if (document.getElementById('actionLeft').innerText == 0) {
+        document.getElementById('submit').style.display = 'none';
+        document.getElementById('actionLeftArea').style.display = 'none';
+        document.getElementById('loserArea').style.display = 'block';
     }
 })
